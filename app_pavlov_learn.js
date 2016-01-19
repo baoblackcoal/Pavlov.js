e43f93e16877c2e8d528587897346294ee5c8d9e
baoblackcoal@hotmail.com <baoblackcoal@hotmail.com>
2016-01-19 08:06:57 +0800

    增加mdp的经验结构体，前状态、前动作、前获取的回报和后状态

---
+++ app_pavlov_learn.js
@@ -0,0 +1,42 @@
+var pavlov = require('./pavlov_learn.js');
+
+//var e = new Experience();
+var e1 = new pavlov.Experience("B", "L", 0, "A");
+var e2 = new pavlov.Experience("A", "L", 1, "Prize");
+var e3 = new pavlov.Experience("B", "B", 0, "D");
+var e4 = new pavlov.Experience("D", "F", 0, "B");
+var e5 = new pavlov.Experience("Prize", "F", 0, "D");
+var e6 = new pavlov.Experience("Prize", "R", 0, "C");
+var e7 = new pavlov.Experience("D", "R", 0, "C");
+var e8 = new pavlov.Experience("C", "F", 0, "A");
+var e9 = new pavlov.Experience("C", "R", 0, "D");
+var e10 = new pavlov.Experience("A", "R", 0, "B");
+
+var e = [];
+e.push(e1);
+e.push(e2);
+e.push(e3);
+e.push(e4);
+e.push(e5);
+e.push(e6);
+e.push(e7);
+e.push(e8);
+e.push(e9);
+e.push(e10);
+
+console.log(pavlov.policy(e));
+/*
+var obs1 = [{state:"A", action:"R"}, {state:"B", action:"B"}];
+var obs2 = [{state:"A", action:"B"}, {state:"C", action:"R"}];
+var obs3 = [{state:"A", action:"L"}, {state:"Prize", action:"R"}, {state:"Trap", action:"F"}];
+var obs4 = [{state:"A", action:"L"}, {state:"Prize", action:"L"},{state:"Trap", action:"B"}];
+var obs5 = [{state:"B",action:"B"},{state:"D",action:"L"}, {state: "C",action:"F"}, {state:"A",action:"R"}];
+var obs6 = [{state:"C",action:"R"},{state:"D",action:"F"},{state:"B", action:"L"}, {state:"A", action:"L"}];
+
+observations = [obs1,obs2,obs3,obs4,obs5,obs6];
+rewards = [0,0,1,1,0,0];
+
+console.log(pavlov.policy(observations,rewards));
+
+// { A: 'L', B: 'L', C: 'F', Prize: 'R', Trap: 'B', D: 'L' }
+*/
---
+++ pavlov_learn.js
@@ -0,0 +1,139 @@
+'use strict';
+
+var Experience = module.exports.Experience = function(state0, action0, reward0, state1) {
+	this.state0 = state0;
+	this.action0 = action0;
+	this.reward0 = reward0;
+	this.state1 = state1;
+}
+
+function countSteps(experience,P_,R_){
+
+  experience.forEach(function(step,i){
+	var stepReward = step.reward0/2;
+
+    //initialization
+    R_[step.state0] = R_[step.state0] || {reward:0, count:0};
+	R_[step.state1] = R_[step.state1] || {reward:0, count:0};
+    P_[step.state0] = P_[step.state0] || {};
+    P_[step.state0][step.action0] = P_[step.state0][step.action0] || {};
+	P_[step.state1] = P_[step.state1] || {};
+
+    //increment total reward count
+    R_[step.state0].reward += stepReward;
+    R_[step.state0].count += 1;
+	R_[step.state1].reward += stepReward;
+    R_[step.state1].count += 1;
+    //and visit count
+     P_[step.state0][step.action0][step.state1] = P_[step.state0][step.action0][step.state1] + 1 || 1;
+  });
+};
+
+function getRewardsFromCount(R_){
+  var R = {};
+ // console.log(R_);
+  Object.keys(R_).forEach(function(state){
+	//console.log("R_:", state, R_[state].reward);
+    R[state] = R_[state].reward / R_[state].count;
+  });
+  return R;
+};
+
+function getTransProbsFromCount(P_){
+  Object.keys(P_).forEach(function(state){
+    Object.keys(P_[state]).forEach(function(action){
+      var visitCount = Object.keys(P_[state][action]).reduce(function(sum,state_){
+        return sum + P_[state][action][state_];
+      },0);
+      Object.keys(P_[state][action]).forEach(function(state_){
+        P_[state][action][state_] = P_[state][action][state_] / visitCount;
+      });
+    });
+  });
+  return P_;
+};
+
+var rewardsAndTransitions = module.exports.rewardsAndTransitions = function(experience){
+
+  var P_ = {};
+  var R_ = {};
+
+  countSteps(experience,P_,R_);
+  //observations.forEach(function(observation,i){
+  //  countSteps(experience,P_,R_);
+  //});
+
+  var R = getRewardsFromCount(R_);
+  var P = getTransProbsFromCount(P_);
+
+  return [P,R];
+};
+
+function isConverged(V,V_){
+  var totalDif = 0;
+  var totalOld = 0;
+  Object.keys(V).forEach(function(state){
+    totalDif += Math.abs(V[state] - V_[state]);
+    totalOld += Math.abs(V_[state]);
+  });
+  return (totalDif < 0.001*totalOld)
+};
+
+function copyObj(obj){
+  var obj_ = {};
+  Object.keys(obj).forEach(function(key){
+    obj_[key] = obj[key];
+  });
+  return obj_;
+};
+
+function policyFormatted(P,R){
+  var policy = {}
+  var V = {};
+  Object.keys(R).forEach(function(state){
+    V[state] = 0;
+  });
+
+  var val;
+  var notConverged = true;
+  var notAction = true;
+  while (notConverged){
+    var V_ = copyObj(V);
+    Object.keys(P).forEach(function(state){
+      var futureVal = -Infinity;
+	  notAction = true;
+      Object.keys(P[state]).forEach(function(action){
+		notAction = false;
+        val = 0;
+//        //assume uniform transition probabilities if no data is available
+//        if (Object.keys(P[state][action]).length == 0){
+//          var states = Object.keys(P);
+//          var uniformProb = 1/states.length;
+//          states.forEach(function(state_){
+//            P[state][action][state_] = uniformProb;
+//          });
+//        }
+        Object.keys(P[state][action]).forEach(function(state_){
+          val += 0.9*(P[state][action][state_] * V[state_]);
+        });
+        if (val > futureVal){
+          futureVal = val;
+          policy[state] = action;
+        }
+        V[state] = R[state] + futureVal;
+      });
+	  if (notAction) V[state] = R[state] + 0.9*V[state];
+    });
+    notConverged = !isConverged(V,V_);
+  };
+  console.log("V:", V);
+
+  return policy;
+};
+
+
+var policy = module.exports.policy = function(experience){
+	var MDP = rewardsAndTransitions(experience);
+	console.log(MDP);
+	return policyFormatted(MDP[0], MDP[1]);
+};
