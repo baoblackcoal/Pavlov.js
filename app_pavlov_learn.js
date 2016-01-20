0c12449e111847d410221733fb0ca9757630ff2a
baoblackcoal@hotmail.com <baoblackcoal@hotmail.com>
2016-01-21 00:09:31 +0800

    1.增加状态后不带动作的动作标识为“NULL”

--- app_pavlov_learn.js
+++ app_pavlov_learn.js
@@ -1,4 +1,4 @@
-var pl = require('./pavlov_learn.js');
+var pavlov_learn = require('./pavlov_learn.js');
 
 //背包问题
 //物品：A B C
@@ -6,44 +6,44 @@ var pl = require('./pavlov_learn.js');
 //价值：30 20 20
 //约束：重量不能大于30
 //目标：价值最大
-//pl.experienceReset();
+//pavlov_learn.experienceReset();
 
 
-//var dqn = new pl.Brain();
+//var dqn = new pavlov_learn.Brain();
 //console.log("dqn=", dqn.getValue());
 
-//var e = new pl.Experience1(1, 2,3,4 );
+//var e = new pavlov_learn.Experience1(1, 2,3,4 );
 //console.log("e=", e);
 
-var e1 = new pl.Experience("B", "L", 0, "A");
-var e2 = new pl.Experience("A", "L", 1, "Prize");
-var e3 = new pl.Experience("B", "B", -1, "D");
-var e4 = new pl.Experience("D", "F", 0, "B");
-var e5 = new pl.Experience("Prize", "L", 0, "A");
-//var e6 = new pl.Experience("D", "L", 1, "Prize");
-var e7 = new pl.Experience("D", "R", 0, "C");
-var e8 = new pl.Experience("C", "F", 0, "A");
-var e9 = new pl.Experience("C", "R", -1, "D");
-var e10 = new pl.Experience("A", "R", 0, "B");
+var e1 = new pavlov_learn.Experience("B", "L", 0, "A");
+var e2 = new pavlov_learn.Experience("A", "L", 1, "Prize");
+var e3 = new pavlov_learn.Experience("B", "B", -1, "D");
+var e4 = new pavlov_learn.Experience("D", "F", 0, "B");
+//var e5 = new pavlov_learn.Experience("Prize", "L", 0, "A");
+//var e6 = new pavlov_learn.Experience("D", "L", 1, "Prize");
+var e7 = new pavlov_learn.Experience("D", "L", 0, "C");
+var e8 = new pavlov_learn.Experience("C", "F", 0, "A");
+var e9 = new pavlov_learn.Experience("C", "R", -1, "D");
+var e10 = new pavlov_learn.Experience("A", "R", 0, "B");
 
-var e1 = new pl.Experience("B", "L", 0, "A");
+var e1 = new pavlov_learn.Experience("B", "L", 0, "A");
 
-var brain = new pl.Brain();
+var brain = new pavlov_learn.Brain();
 brain.experienceReset();
 var e = [];
 e.push(e1);
 e.push(e2);
 e.push(e3);
-e.push(e4);
-e.push(e5);
 console.log(brain.policy(e));
+
 var e = [];
+e.push(e4);
+//e.push(e5);
 //e.push(e6);
 e.push(e1);
 e.push(e2);
 e.push(e3);
 e.push(e4);
-e.push(e5);
 e.push(e7);
 e.push(e8);
 e.push(e9);
@@ -51,15 +51,15 @@ e.push(e10);
 console.log(brain.policy(e));
 
 /*
-pl.experienceReset();
-var e1 = new pl.Experience({a:1, b:1}, 1, 0, {a:2, b:2});
-var e2 = new pl.Experience({a:2, b:2}, 1, 1, {a:3, b:3});
-var e3 = new pl.Experience({a:3, b:3}, -1, 1, {a:2, b:2});
+pavlov_learn.experienceReset();
+var e1 = new pavlov_learn.Experience({a:1, b:1}, 1, 0, {a:2, b:2});
+var e2 = new pavlov_learn.Experience({a:2, b:2}, 1, 1, {a:3, b:3});
+var e3 = new pavlov_learn.Experience({a:3, b:3}, -1, 1, {a:2, b:2});
 var e = [];
 e.push(e1);
 e.push(e2);
 e.push(e3);
-var p = pl.policy(e)
+var p = pavlov_learn.policy(e)
 console.log(p);
 */
 
@@ -97,22 +97,22 @@ var state0 = 1, action =1;
 
 state0 = 1, action =2;
 ret = state0ActionToState1(state0, action)
-var e1 = new pl.Experience(state0, action, ret[0], ret[1]);
+var e1 = new pavlov_learn.Experience(state0, action, ret[0], ret[1]);
 state0 = 1, action =1;
 ret = state0ActionToState1(state0, action)
-var e2 = new pl.Experience(state0, action, ret[0], ret[1]);
+var e2 = new pavlov_learn.Experience(state0, action, ret[0], ret[1]);
 state0 = 3, action =2;
 ret = state0ActionToState1(state0, action)
-var e3 = new pl.Experience(state0, action, ret[0], ret[1]);
+var e3 = new pavlov_learn.Experience(state0, action, ret[0], ret[1]);
 state0 = 4, action =1;
 ret = state0ActionToState1(state0, action)
-var e4 = new pl.Experience(state0, action, ret[0], ret[1]);
+var e4 = new pavlov_learn.Experience(state0, action, ret[0], ret[1]);
 state0 = 4, action =-1;
 ret = state0ActionToState1(state0, action)
-var e5 = new pl.Experience(state0, action, ret[0], ret[1]);
+var e5 = new pavlov_learn.Experience(state0, action, ret[0], ret[1]);
 state0 = 3, action =-2;
 ret = state0ActionToState1(state0, action)
-var e6 = new pl.Experience(state0, action, ret[0], ret[1]);
+var e6 = new pavlov_learn.Experience(state0, action, ret[0], ret[1]);
 //console.log(e1);
 
 var e = [];
@@ -122,5 +122,5 @@ e.push(e3);
 e.push(e4);
 e.push(e5);
 e.push(e6);
-console.log(pl.policy(e));
+console.log(pavlov_learn.policy(e));
  */
--- pavlov_learn.js
+++ pavlov_learn.js
@@ -1,6 +1,3 @@
-'use strict';
-
-
 var pavlov_learn = pavlov_learn || { REVISION: 'ALPHA' };
 var experienceR = {};
 var experienceP = {};
@@ -14,9 +11,7 @@ var experienceP = {};
     this.state1 = state1;
   }
 
-  var Brain = function(){
-    this.value = 222222222;
-  }
+  var Brain = function(){};
   Brain.prototype = {
     experienceReset: function () {
       experienceR = {};
@@ -156,18 +151,22 @@ var experienceP = {};
             }
             V[state] = R[state] + futureVal;
           });
-          if (notAction) V[state] = R[state] + r * V[state];
+          if (notAction) {
+            V[state] = R[state] + r * V[state];
+            policy[state] = policy[state] || "NULL";
+          }
         });
 
         notConverged = !this.isConverged(V, V_);
+
 //	if (cnt > 10) {
 //		console.log("cnt=",cnt);
 //		break;
 //	}
       }
 
-      console.log("V:", V);
-      console.log("cnt=", cnt);
+      console.log("V=", V);
+      console.log("Loop cnt=", cnt);
 
       return policy;
     },
@@ -191,182 +190,3 @@ var experienceP = {};
     module.exports = lib; // in nodejs
   }
 })(pavlov_learn);
-
-/*
-
-var experienceR = {};
-var experienceP = {};
-
-var Experience = module.exports.Experience = function(state0, action0, reward0, state1) {
-	this.state0 = state0;
-	this.action0 = action0;
-	this.reward0 = reward0;
-	this.state1 = state1;
-}
-
-var experienceReset = module.exports.experienceReset = function() {
-	experienceR = {};
-	experienceP = {};
-}
-
-function countSteps(experience,P_,R_){
-
-  experience.forEach(function(step,i){
-	var stepReward = step.reward0/2;
-	var state0Replace, state1Replace;
-	state0Replace = step.state0;
-	state1Replace = step.state1;
-
-	if(typeof step.state0 === "object") {
-		state0Replace="{ ", state1Replace="{ ";
-		Object.keys(step.state0).forEach(function(i){
-			state0Replace += step.state0[i];
-			state0Replace += " ";
-			state1Replace += step.state1[i];
-			state1Replace += " ";
-		});
-		state0Replace += "}";
-		state1Replace += "}";
-//		console.log("state0Replace:",state0Replace);
-//		console.log("state1Replace:",state1Replace);
-	}
-
-    //initialization
-    R_[state0Replace] = R_[state0Replace] || {reward:0, count:0};
-	R_[state1Replace] = R_[state1Replace] || {reward:0, count:0};
-    P_[state0Replace] = P_[state0Replace] || {};
-    P_[state0Replace][step.action0] = P_[state0Replace][step.action0] || {};
-	P_[state1Replace] = P_[state1Replace] || {};
-
-    //increment total reward count
-    R_[state0Replace].reward += stepReward;
-    R_[state0Replace].count += 1;
-	R_[state1Replace].reward += stepReward;
-    R_[state1Replace].count += 1;
-    //and visit count
-     P_[state0Replace][step.action0][state1Replace] = P_[state0Replace][step.action0][state1Replace] + 1 || 1;
-  });
-};
-
-function getRewardsFromCount(R_){
-  var R = {};
- // console.log(R_);
-  Object.keys(R_).forEach(function(state){
-	//console.log("R_:", state, R_[state].reward);
-	experienceR[state] = experienceR[state] || 0;
-    R[state] = R_[state].reward / R_[state].count;
-	experienceR[state] += R[state];
-  });
-//  return R;
-  return experienceR;
-};
-
-function getTransProbsFromCount(P_){
-  Object.keys(P_).forEach(function(state){
-	  experienceP[state] = experienceP[state] || {};
-    Object.keys(P_[state]).forEach(function(action){
-		experienceP[state][action] = experienceP[state][action] || {};
-      var visitCount = Object.keys(P_[state][action]).reduce(function(sum,state_){
-        return sum + P_[state][action][state_];
-      },0);
-      Object.keys(P_[state][action]).forEach(function(state_){
-		experienceP[state][action][state_] = experienceP[state][action][state_] || 0;
-        P_[state][action][state_] = P_[state][action][state_] / visitCount;
-		experienceP[state][action][state_] += P_[state][action][state_];
-//		console.log(experienceP);
-	  });
-    });
-  });
-//  return P_;
-	return experienceP;
-};
-
-var rewardsAndTransitions = module.exports.rewardsAndTransitions = function(experience){
-
-  var P_ = {};
-  var R_ = {};
-
-  countSteps(experience,P_,R_);
-  //observations.forEach(function(observation,i){
-  //  countSteps(experience,P_,R_);
-  //});
-
-  var R = getRewardsFromCount(R_);
-  var P = getTransProbsFromCount(P_);
-
-  return [P,R];
-};
-
-function isConverged(V,V_){
-  var totalDif = 0;
-  var totalOld = 0;
-  Object.keys(V).forEach(function(state){
-    totalDif += Math.abs(V[state] - V_[state]);
-    totalOld += Math.abs(V_[state]);
-  });
-//  console.log("totalDif:",totalDif,"totalOld:",0.001*totalOld);
-  return (totalDif < 0.001*totalOld)
-};
-
-function copyObj(obj){
-  var obj_ = {};
-  Object.keys(obj).forEach(function(key){
-    obj_[key] = obj[key];
-  });
-  return obj_;
-};
-
-function policyFormatted(P,R){
-  var policy = {}
-  var V = {};
-  Object.keys(R).forEach(function(state){
-    V[state] = 0;
-  });
-
-  var val;
-  var notConverged = true;
-  var notAction = true;
-  var cnt = 0;
-  var futureVal;
-  var r = 0.333;
-  while (notConverged){	
-    var V_ = copyObj(V);
-	cnt++;
-    Object.keys(P).forEach(function(state){
-      futureVal = -Infinity;
-	  notAction = true;
-      Object.keys(P[state]).forEach(function(action){
-        val = 0;
-        Object.keys(P[state][action]).forEach(function(state_){
-          val += r*(P[state][action][state_] * V[state_]);
-        });
-		//console.log("val=", val);
-
-        if (val > futureVal){
-          futureVal = val;
-          policy[state] = action;
-        }
-		V[state] = R[state] + futureVal;
-      });
-	  if (notAction) V[state] = R[state] + r*V[state];
-    });
-	
-	notConverged = !isConverged(V,V_);
-//	if (cnt > 10) {
-//		console.log("cnt=",cnt);
-//		break;
-//	}
-  };
-  console.log("V:", V);
-  console.log("cnt=",cnt);
-
-  return policy;
-};
-
-
-var policy = module.exports.policy = function(experience){
-	var MDP = rewardsAndTransitions(experience);
-	//console.log(MDP[0]);
-	return policyFormatted(MDP[0], MDP[1]);
-};
-*/
